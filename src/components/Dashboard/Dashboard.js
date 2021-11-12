d785278b644dbee61cd2f827a033c692346450f3
Vimal Saifudin <vimalsaifudin@gmail.com>
2021-11-12 15:05:37 +0400

    Few minor corrections

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -40,13 +40,13 @@ class Dashboard extends React.Component {
       }
       )
     })
-    .then(response => response.json())
-    .then((json) => {
-      this.setState({ 
+      .then(response => response.json())
+      .then((json) => {
+        this.setState({
           apiResponse: [json]
         })
       }
-    )  
+      )
     resetForm()
 
   }
@@ -66,118 +66,110 @@ class Dashboard extends React.Component {
       <div className="wrapper">
         <h1>Purchase Reimbursement Form</h1>
         <div className="purchase-form">
-        <Formik
-          initialValues={initialValues}
-          validationSchema={this.validationSchema}
-          onSubmit={this.handleSubmit}>
-          {
-          ({
-              values,
-              handleChange,
-              setFieldValue,
-              errors,
-              touched,
-              handleBlur,
-              isValid,
-              dirty
-            }) => (
-            
-          <Form>
-            <label>
-              <p>Purchase Date <span style={{color: 'red'}} >*</span></p>
-              <DatePicker 
-              name="purchaseDate"
-              onChange={setFieldValue}
-              value={values.purchaseDate } 
+          <Formik
+            initialValues={initialValues}
+            validationSchema={this.validationSchema}
+            onSubmit={this.handleSubmit}>
+            {
+              ({
+                values,
+                handleChange,
+                setFieldValue,
+                errors,
+                touched,
+                handleBlur,
+                isValid,
+                dirty
+              }) => (
 
-              />
-            </label>
-            <ErrorMessage
-                  name="purchaseDate"
-                  component="div"
-                  className="text-danger"
-                />
-            <label>
-              <p>Purchase Amount <span style={{color: 'red'}} >*</span></p>
-              <input 
-              type="number" 
-              name="purchaseAmount" 
-              onChange={handleChange} 
-              onBlur={handleBlur} 
-              value={values.purchaseAmount }    />
-            </label>
-            <ErrorMessage
-                  name="purchaseAmount"
-                  component="div"
-                  className="text-danger"
-                />
-            <label>
-              <p>Purchase Description</p>
-              <textarea 
-              name="purchaseDescription"
-              value={values.purchaseDescription}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              ></textarea>
-            </label>
+                <Form>
+                  <label>
+                    <p>Purchase Date <span style={{ color: 'red' }} >*</span></p>
+                    <DatePicker
+                      name="purchaseDate"
+                      onChange={setFieldValue}
+                      value={values.purchaseDate}
 
-            <label>
-              <p>Upload Receipt (PDF/Images)<span style={{color: 'red'}} >*</span></p>
-              <input 
-              name="purchaseReceipt" 
-              type="file" 
-              accept="application/pdf, image/*"
-              onChange={(event) => {
-                setFieldValue("purchaseReceipt", event.currentTarget.files[0]);
-              }}
-               />
-            </label>
-            <ErrorMessage
-                  name="purchaseReceipt"
-                  component="div"
-                  className="text-danger"
-                  style={{marginBottom: '10px'}}
-                />
+                    />
+                  </label>
+                  <ErrorMessage
+                    name="purchaseDate"
+                    component="div"
+                    className="text-danger"
+                  />
+                  <label>
+                    <p>Purchase Amount <span style={{ color: 'red' }} >*</span></p>
+                    <input
+                      type="number"
+                      name="purchaseAmount"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.purchaseAmount} />
+                  </label>
+                  <ErrorMessage
+                    name="purchaseAmount"
+                    component="div"
+                    className="text-danger"
+                  />
+                  <label>
+                    <p>Purchase Description</p>
+                    <textarea
+                      name="purchaseDescription"
+                      value={values.purchaseDescription}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    ></textarea>
+                  </label>
 
+                  <label>
+                    <p>Upload Receipt (PDF/Images)<span style={{ color: 'red' }} >*</span></p>
+                    <input
+                      name="purchaseReceipt"
+                      type="file"
+                      accept="application/pdf, image/*"
+                      onChange={(event) => {
+                        setFieldValue("purchaseReceipt", event.currentTarget.files[0]);
+                      }}
+                    />
+                  </label>
+                  <ErrorMessage
+                    name="purchaseReceipt"
+                    component="div"
+                    className="text-danger"
+                    style={{ marginBottom: '10px' }}
+                  />
 
-            <div>
-              <input type="submit" />
-            </div>
-          </Form>
-          )}
+                  <div>
+                    <input type="submit" />
+                  </div>
+                </Form>
+              )}
           </Formik>
-          
-
-{apiResponse && apiResponse.length > 0 &&
-        <div className="responseText">
-        <h2>Your purchase reimbursement request has been successfully submitted:</h2>
-        {
-          apiResponse.map((item) => ( 
-            <table key = { item.purchaseAmount } >
-              <tr><td>Purchase Date: </td><td>{ moment(item.purchaseDate).format('MM/DD/YYYY') }</td></tr>
-              <tr><td>Purchase Amount: </td><td>{ item.purchaseAmount }</td></tr>
-              <tr><td>Purchase Description: </td><td>{ item.purchaseDescription }</td></tr>
-              {/* <tr><td>Purchase Receipt</td><td>{ item.receiptFile }</td></tr> */}
-              <tr><td>Purchase Receipt File Name: </td><td>{ item.receiptFileName }</td></tr>
-              <tr><td>Purchase Receipt File Type: </td><td>{ item.receiptFileType }</td></tr>
-              <tr><td>Purchase Receipt File Size: </td><td>{ item.receiptFileSize }</td></tr>
-            </table>
-            ))
-        }
-
-        
-      </div>
-
-}
 
 
+          {apiResponse && apiResponse.length > 0 &&
+            <div className="responseText">
+              <h2>Your purchase reimbursement request has been successfully submitted:</h2>
+              {
+                apiResponse.map((item) => (
+                  <table key={item.purchaseAmount} >
+                    <tr><td>Purchase Date: </td><td>{moment(item.purchaseDate).format('MM/DD/YYYY')}</td></tr>
+                    <tr><td>Purchase Amount: </td><td>{item.purchaseAmount}</td></tr>
+                    <tr><td>Purchase Description: </td><td>{item.purchaseDescription}</td></tr>
+                    <tr><td>Purchase Receipt File Name: </td><td>{item.receiptFileName}</td></tr>
+                    <tr><td>Purchase Receipt File Type: </td><td>{item.receiptFileType}</td></tr>
+                    <tr><td>Purchase Receipt File Size: </td><td>{item.receiptFileSize}</td></tr>
+                  </table>
+                ))
+              }
 
 
+            </div>
 
-
+          }
 
         </div>
-        
+
       </div>
     );
   }
